138fc55c1b44c76479383b205801064486a93799
evgeniya dudchenko <evgeniya.dudchenko.04@gmail.com>
2023-06-25 20:52:13 +0200

    add docs (with mobile)

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -15,6 +15,8 @@ export class MainPageComponent {
   private _intervals: any[] = [];
   public isLightTheme = true;
   public showNextComponent: boolean = false;
+  public isMobile = false;
+  public isOpenMenu = false;
 
   constructor(public translate: TranslateService, private ngZone: NgZone) {
     translate.addLangs(['eng', 'pl', 'ua']);
@@ -22,6 +24,7 @@ export class MainPageComponent {
     this.ngZone.runOutsideAngular(() => {
       window.addEventListener('wheel', this.onWheel, { passive: false });
     });
+    this.setIsMobile();
   }
 
   ngOnInit(): void {
@@ -30,6 +33,21 @@ export class MainPageComponent {
     }, 1000);
 
     this._timeouts.push(typeTimeout);
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
+
+  private setIsMobile() {
+    let screen = window.screen;
+    this.ngZone.run(() => {
+      this.isMobile = screen.height > screen.width;
+    });
+  }
+
+  public toggleMenu() {
+      this.isOpenMenu = !this.isOpenMenu;
   }
 
   onChangeLanguage(language: string): void {
@@ -64,7 +82,7 @@ export class MainPageComponent {
       const line = this.textLine.nativeElement as HTMLElement;
       line.textContent = '';
       let index = 0;
-  
+
       const typeInterval = setInterval(() => {
         if (index <= this._texts[this._currentIndex].length) {
           line.textContent = this._texts[this._currentIndex].substring(0, index);
@@ -72,16 +90,16 @@ export class MainPageComponent {
         } else {
           clearInterval(typeInterval);
           const eraseTimeout = setTimeout(() => this.eraseLine(line), 1000);
-  
+
           // Store the timeout in the array
           this._timeouts.push(eraseTimeout);
         }
       }, 100);
-  
+
       line.style.opacity = '1';
     }
   }
-  
+
   eraseLine(line: HTMLElement): void {
     const eraseInterval = setInterval(() => {
       if (line.textContent!.length > 0) {
@@ -90,35 +108,35 @@ export class MainPageComponent {
         clearInterval(eraseInterval);
         line.style.opacity = '0';
         this._currentIndex++;
-  
+
         if (this._currentIndex < this._texts.length) {
           const typeTimeout = setTimeout(() => this.typeLines(), 500);
-  
+
           // Store the timeout in the array
           this._timeouts.push(typeTimeout);
         }
       }
     }, 50);
-  
+
     if (this._currentIndex + 1 >= this._texts.length) {
       const showNextComponentTimeout = setTimeout(() => {
         this.showNextComponent = true;
       }, 1500);
-  
+
       // Store the timeout in the array
       this._timeouts.push(showNextComponentTimeout);
     }
   }
-  
+
   onSkipIntro() {
     // Clear all _timeouts and intervals
     this._timeouts.forEach(timeout => clearTimeout(timeout));
     this._intervals.forEach(interval => clearInterval(interval));
-  
+
     // Reset the arrays
     this._timeouts = [];
     this._intervals = [];
-  
+
     this.showNextComponent = true;
   }
 
